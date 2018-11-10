aa3cad36365ae043565a2c44b944282c88084fc3
Judson Stevens <Judson.Stevens@gmail.com>
2018-11-10 07:57:17 -0700

    Added Jupyter notebook integration to allow graphs inside VS Code to look at correlations

--- example.py
+++ example.py
@@ -7,8 +7,14 @@ from scipy import stats
 
 train_df = pd.read_csv('DenverHousingData.csv')
 train_df = train_df.drop(['latitude', 'longitude', 'full_address', 'comparables_link', 'use_code',
-                          'tax_assessment_year', 'tax_assessment_amount', 'last_sold_date', 'region_name', 'region_type', 'zindex_value_of_region'], axis=1)
-train_df = train_df.dropna() 
+                          'tax_assessment_year', 'last_sold_date', 'region_name', 'region_type', 
+                          'zindex_value_of_region'], axis=1)
+# Drop the rows that contain any NA's
+train_df = train_df.dropna()
+# Drop the rows that contain any values in any column equal to 0
+train_df = train_df[(train_df != 0).all(1)]
+# for column in train_df.columns.values.tolist():
+#   train_df[f'{column}'].replace(0, train_df[f'{column}'].mean())
 
 train_df = train_df[np.abs(train_df.living_area - train_df.living_area.mean()) <= (3*train_df.living_area.std())]
 train_df = train_df[np.abs(train_df.last_sold_price - train_df.last_sold_price.mean()) <= (3*train_df.last_sold_price.std())]
@@ -16,9 +22,9 @@ train_df = train_df[np.abs(train_df.year_built - train_df.year_built.mean()) <= 
 # train_df = train_df[(np.abs(stats.zscore(train_df)) < 3).all(axis=1)]
 
 plt.figure(figsize=(12,12))
-plt.scatter(train_df['last_sold_price'], train_df['year_built'])
-plt.ylabel('Year Built', fontsize=18)
-plt.xlabel('Last Sold Price', fontsize=18)
+plt.scatter(train_df['zestimate_amount'], train_df['living_area'])
+plt.ylabel('Living Area', fontsize=18)
+plt.xlabel('Z Estimate Amount', fontsize=18)
 plt.show()
 
 
