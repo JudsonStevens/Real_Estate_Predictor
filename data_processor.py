8ce956a61bede8e55b2f6e5b43a592fbbf4084fa
Judson Stevens <Judson.Stevens@gmail.com>
2019-09-24 18:08:50 -0600

    Working on different models for data - currently working on regularized linear models

--- data_processor.py
+++ data_processor.py
@@ -4,61 +4,67 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.layers import Dense
 from keras import Sequential
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from scipy.stats import skew
 
 def main():
-  train_df = pd.read_csv('DenverHousingData.csv', index_col='zillow_id')
+  train_df = pd.read_csv('DenverHousingData.csv')
   train_df = train_df.drop(['latitude', 'longitude', 'full_address', 'comparables_link', 'use_code',
-                            'tax_assessment_year', 'tax_assessment_amount', 'last_sold_date', 'last_sold_price', 
-                            'region_name', 'region_type', 'zindex_value_of_region'], axis=1)
+                            'tax_assessment_year', 'last_sold_date', 'region_name', 'region_type', 
+                            'zindex_value_of_region', 'zillow_id'], axis=1)
+  # Drop the rows that contain any NA's
   train_df = train_df.dropna()
-  train_df = train_df[np.abs(train_df.living_area - train_df.living_area.mean()) <= (3*train_df.living_area.std())]
-  train_df = train_df.dropna()
-  # print(train_df.head(1))
+  # Drop the rows that contain any values in any column equal to 0
+  train_df = train_df[(train_df != 0).all(1)]
+  # Drop a row that happened to contain the text zestimate_amount for some reason
+  train_df = train_df[~train_df['zestimate_amount'].isin(['zestimate_amount'])]
+  # Turn all of the values to floats
+  train_df = train_df.astype(float)
 
-  target = 'zestimate_amount'
+  def drop_outliers(df, field_name):
+      distance = 1.5 * (np.percentile(df[field_name], 85) - np.percentile(df[field_name], 15))
+      df.drop(df[df[field_name] > distance + np.percentile(df[field_name], 85)].index, inplace=True)
+      df.drop(df[df[field_name] < np.percentile(df[field_name], 35) - distance].index, inplace=True)
 
-  print(train_df.head(1))
-  new_train = train_df.drop(target, axis=1)
-  scaler = MinMaxScaler(feature_range=(0, 1))
-  y_scaler = MinMaxScaler(feature_range=(0, 1))
-  scaled_train_df = scaler.fit_transform(new_train)
-  scaled_train_2 = y_scaler.fit_transform(train_df[[target]])
+  columns = ['tax_assessment_amount', 'year_built', 'lot_size_sq_ft', 'living_area', 'bathrooms', 'total_rooms', 'last_sold_price', 'zestimate_amount']
 
-  # new = pd.DataFrame(scaled_train, columns = train_df.columns)
-  # print(new.head(10))
-  # print(f"Note: median values were scaled by multiplying by {scaler.scale_[5]} and adding {scaler.min_[5]}.")
-  # multiplied_by = scaler.scale_[5]
-  # added = scaler.min_[5]
+  for i in range(len(columns)):
+    drop_outliers(train_df, columns[i])
+    i += 1
 
-  scaled_train_df = pd.DataFrame(scaled_train_df, columns = new_train.columns)
-  scaled_train_2 = pd.DataFrame(scaled_train_2)
+  train_target = train_df.zestimate_amount
+  train_predictors = train_df.drop('zestimate_amount', axis=1)
 
-  y = scaled_train_2.values
-  x = scaled_train_df.values
-  model = Sequential()
+  predictor_train, predictor_test, target_train, target_test = train_test_split(train_predictors, train_target, test_size=0.3)
 
-  model.add(Dense(50, input_dim = 5, activation = 'relu'))
-  model.add(Dense(70, activation = 'relu'))
-  model.add(Dense(70, activation = 'relu'))
-  model.add(Dense(50, activation = 'relu'))
-  model.add(Dense(1, activation = 'sigmoid'))
+  # This will transform the target variable using the log function
+  train_target = np.log1p(train_target)
 
-  model.compile(loss='mean_squared_error', optimizer='adam')
+  # This will transfer the skewed predictor variables. 
+  skewed_feats = train_predictors.apply(lambda x: skew(x.dropna()))
+  skewed_feats = skewed_feats[skewed_feats > .75]
+  skewed_feats = skewed_feats.index
+
+  train_predictors = np.log1p(train_predictors[skewed_feats])
 
 
-  model.fit(x[100:, :], y[100:, :], epochs = 500, shuffle = True, verbose = 2)
-  # y2 = y[:2]
-  # y2 -= added
-  # y2 /= multiplied_by
-  # print(y2)
-  # print(train_df[:2])
-  prediction = model.predict(x[:1, :])
-  print(y_scaler.inverse_transform(prediction)[0][0])
-  import code; code.interact(local=dict(globals(), **locals()))
-  # y_0 = prediction[0][0]
-  # y_0 -= added
-  # y_0 /= multiplied_by
 
+
+
+  # print(x_train.shape, y_train.shape)
+  # print(x_test.shape, y_test.shape)
+
+  # my_model = RandomForestRegressor()
+  # my_model.fit(x_train, y_train)
+
+  # predicted_prices = my_model.predict(x_test)
+  # score = my_model.score(x_test, y_test)
+
+  # print(predicted_prices)
+  # print(score)
 
 
 if __name__ == "__main__":
--- eda.py
+++ eda.py
@@ -14,14 +14,10 @@ train_df = train_df.drop(['latitude', 'longitude', 'full_address', 'comparables_
 train_df = train_df.dropna()
 # Drop the rows that contain any values in any column equal to 0
 train_df = train_df[(train_df != 0).all(1)]
+# Drop a row that happened to contain the text zestimate_amount for some reason
 train_df = train_df[~train_df['zestimate_amount'].isin(['zestimate_amount'])]
+# Turn all of the values to floats
 train_df = train_df.astype(float)
-
-# df_list = list(train_df)
-# minPercentile = 0.15
-# maxPercentile = 0.85
-# for _ in range(8):
-#     train_df[df_list[_]] = train_df[df_list[_]].clip((train_df[df_list[_]].quantile(minPercentile)),(train_df[df_list[_]].quantile(maxPercentile)))
 
 def drop_outliers(df, field_name):
     distance = 1.5 * (np.percentile(df[field_name], 85) - np.percentile(df[field_name], 15))
@@ -38,17 +34,11 @@ print(train_df.dtypes)
 print(train_df.head(5))
 print(train_df.describe())
 
-
-
-# print(train_df.zestimate_amount.unique())
-# print(train_df.zestimate_amount.value_counts())
-# constrains = train_df.select_dtypes(include=[np.number]).apply(lambda x: np.abs(stats.zscore(x)) < 3, reduce=False).all()
-# train_df.drop(train_df.index[~constrains], inplace=True)
-# train_df[(np.abs(stats.zscore(train_df)) < 3).all(axis=1)]?
-# try:
+# Correlation heatmap of the data
 plt.figure(figsize=(6,4))
 sns.heatmap(train_df.corr(),cmap='Blues',annot=False) 
 
+# Box plot of the data
 l = train_df.columns.values
 number_of_columns=8
 number_of_rows = len(l)-1/number_of_columns
@@ -58,22 +48,17 @@ for i in range(0,len(l)):
     sns.set_style('whitegrid')
     sns.boxplot(train_df[l[i]],color='green',orient='v')
     plt.tight_layout()
-# k = 8
-# cols = train_df.corr().nlargest(k, 'zestimate_amount')['zestimate_amount'].index
-# cm = train_df[cols].corr()
-# plt.figure(figsize=(8,6))
-# sns.heatmap(cm, annot=True, cmap='viridis')
-# except ValueError:
-#   pass
+
+# Scatter plot of two pieces of the data
 plt.figure(figsize=(18,18))
-plt.scatter(train_df['zestimate_amount'], train_df['living_area'])
+plt.scatter(train_df['zestimate_amount'], train_df['lot_size_sq_ft'])
 x_label = plt.ylabel('Lot Size in Sq Ft', fontsize=18, color='black')
 y_label = plt.xlabel('Z Estimate Amount', fontsize=18, color='black')
-# plt.axis([min(train_df['zestimate_amount']), max(train_df['zestimate_amount']), min(train_df['living_area']), max(train_df['living_area'])])
 plt.tick_params(axis='x', colors='black')
 plt.tick_params(axis='y', colors='black')
-# plt.ticklabel_format(style='plain', axis='x')
-# [i.set_color("white") for i in plt.gca().get_xticklabels()]
-# [i.set_color("white") for i in plt.gca().get_yticklabels()]
 plt.show()
+
+# Joint plot of two pieces of the data
+sns.jointplot(x=train_df['living_area'], y=train_df['zestimate_amount'], kind='reg')
+
 #%%
